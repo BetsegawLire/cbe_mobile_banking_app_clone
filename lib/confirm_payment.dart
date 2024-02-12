75553156e22f87c42291ff2cdce0b91e0d001385
BetsegawLire <betsegawhazard@gmail.com>
2024-02-12 12:44:32 +0300

    add the qrcode

--- lib/confirm_payment.dart
+++ lib/confirm_payment.dart
@@ -74,150 +74,177 @@ class _ConfirmAlertState extends State<ConfirmAlert> {
   Widget build(BuildContext context) {
     return SizedBox(
       width: MediaQuery.of(context).size.width * 0.9,
-      child: Center(
-        child: AlertDialog(
-          titlePadding: EdgeInsets.symmetric(vertical: 0),
-          buttonPadding: EdgeInsets.symmetric(vertical: 10),
-          actionsPadding: EdgeInsets.symmetric(vertical: 10),
-          // surfaceTintColor: Colors.purple,
-          shape: RoundedRectangleBorder(
-            borderRadius: BorderRadius.circular(0), // Set border radius
-          ),
-          title: Container(
-            padding: EdgeInsets.only(top: 10, bottom: 20, left: 10),
-            color: Colors.lightGreen, // Set purple background color
-            // child: Text(
-            //   "Transfer",
-            //   textAlign: TextAlign.center,
-            //   style: TextStyle(
-            //       color: Colors.white, fontWeight: FontWeight.bold, fontSize: 20),
-            // ),
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: [
-                Row(
-                  children: [
-                    Icon(
-                      Icons.approval,
-                      color: Colors.white,
-                      size: 40,
-                    ),
-                    SizedBox(
-                      width: 5,
-                    ),
-                    Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: [
-                        SizedBox(
-                          width: 70,
-                          child: Text(
-                            "Thank You!",
-                            style: TextStyle(
-                              fontWeight: FontWeight.bold,
-                              fontSize: 22,
-                              color: Colors.white,
-                            ),
-                          ),
-                        ),
-                        Text(
-                          "Success",
-                          style: TextStyle(
-                            color: Colors.grey[400],
-                            fontSize: 14,
-                          ),
-                        ),
-                      ],
-                    )
-                  ],
-                ),
-                Row(
-                  // mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                  children: [
-                    IconButton(
-                      onPressed: () {},
-                      icon: Icon(
-                        Icons.share,
-                        color: Colors.white,
-                      ),
-                    ),
-                    IconButton(
-                      onPressed: () {},
-                      icon: Icon(
-                        Icons.download,
-                        color: Colors.white,
-                      ),
-                    ),
-                    IconButton(
-                      onPressed: () {
-                        Navigator.pop(context);
-                        Navigator.push(context, MaterialPageRoute(
-                          builder: (context) {
-                            return HomeScreen();
-                          },
-                        ));
-                      },
-                      icon: Icon(
-                        Icons.close,
-                        color: Colors.white,
-                      ),
-                    ),
-                  ],
-                ),
-              ],
-            ),
-          ),
-          content: Column(
-            mainAxisSize: MainAxisSize.min,
-            crossAxisAlignment: CrossAxisAlignment.start,
+      child: AlertDialog(
+        titlePadding: EdgeInsets.symmetric(vertical: 0),
+        buttonPadding: EdgeInsets.symmetric(vertical: 10),
+        actionsPadding: EdgeInsets.symmetric(vertical: 10),
+        // surfaceTintColor: Colors.purple,
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(0), // Set border radius
+        ),
+        title: Container(
+          padding: EdgeInsets.only(top: 10, bottom: 20, left: 10),
+          color: Colors.lightGreen, // Set purple background color
+          // child: Text(
+          //   "Transfer",
+          //   textAlign: TextAlign.center,
+          //   style: TextStyle(
+          //       color: Colors.white, fontWeight: FontWeight.bold, fontSize: 20),
+          // ),
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
             children: [
-              Text(
-                "Message",
-                style: TextStyle(color: Colors.grey),
-              ),
-              // SizedBox(height: 16),
-              Text(
-                "ETB ${widget.amount} debited from ${widget.accountName}-ETB-$lastFourCharacters for 364831996497 (A2A) on ${DateTime.now().day}-${getMonthName(DateTime.now().month)}-${DateTime.now().year} with transaction ID: FT24006Z70N6.",
-                style: TextStyle(fontWeight: FontWeight.bold),
-              ),
-              SizedBox(
-                height: 40,
-              ),
-              Container(
-                // height: 0,
-                decoration: BoxDecoration(
-                    color: Colors.grey[300],
-                    borderRadius: BorderRadius.circular(5)),
-                child: ListTile(
-                  contentPadding: EdgeInsets.symmetric(
-                    horizontal: 0,
+              Row(
+                children: [
+                  Icon(
+                    Icons.approval,
+                    color: Colors.white,
+                    size: 40,
                   ),
-                  // leading: Icon(
-                  //   Icons.balance,
-                  //   size: 40,
-                  // )
-                  // ,
-                  leading: SizedBox(
-                    width: 40,
-                    child: Image.asset(
-                      "images/cbe_logo.png",
-                      fit: BoxFit.fill,
+                  SizedBox(
+                    width: 5,
+                  ),
+                  Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      SizedBox(
+                        width: 70,
+                        child: Text(
+                          "Thank You!",
+                          style: TextStyle(
+                            fontWeight: FontWeight.bold,
+                            fontSize: 22,
+                            color: Colors.white,
+                          ),
+                        ),
+                      ),
+                      Text(
+                        "Success",
+                        style: TextStyle(
+                          color: Colors.grey[400],
+                          fontSize: 14,
+                        ),
+                      ),
+                    ],
+                  )
+                ],
+              ),
+              Row(
+                // mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: [
+                  IconButton(
+                    onPressed: () {},
+                    icon: Icon(
+                      Icons.share,
+                      color: Colors.white,
                     ),
                   ),
-                  title: Text(
-                    "Commercial Bank of Ethiopia",
-                    style: TextStyle(fontWeight: FontWeight.bold, fontSize: 14),
-                  ),
-                  subtitle: Text(
-                    "The Bank You always Rely on!",
-                    style: TextStyle(
-                      color: Colors.grey,
-                      fontSize: 12,
+                  IconButton(
+                    onPressed: () {},
+                    icon: Icon(
+                      Icons.download,
+                      color: Colors.white,
                     ),
                   ),
-                ),
-              )
+                  IconButton(
+                    onPressed: () {
+                      Navigator.pop(context);
+                      Navigator.push(context, MaterialPageRoute(
+                        builder: (context) {
+                          return HomeScreen();
+                        },
+                      ));
+                    },
+                    icon: Icon(
+                      Icons.close,
+                      color: Colors.white,
+                    ),
+                  ),
+                ],
+              ),
             ],
           ),
+        ),
+        content: Column(
+          mainAxisSize: MainAxisSize.min,
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Text(
+              "Message",
+              style: TextStyle(color: Colors.grey),
+            ),
+            // SizedBox(height: 16),
+            Text(
+              "ETB ${widget.amount} debited from ${widget.accountName}-ETB-$lastFourCharacters for 364831996497 (A2A) on ${DateTime.now().day}-${getMonthName(DateTime.now().month)}-${DateTime.now().year} with transaction ID: FT24006Z70N6.",
+              style: TextStyle(fontWeight: FontWeight.bold),
+            ),
+            Padding(
+              padding: EdgeInsets.symmetric(horizontal: 10),
+              child: Card(
+                  child: Padding(
+                padding: const EdgeInsets.all(20.0),
+                child: Image.asset("images/qr_code.jpg"),
+              )),
+            ),
+            SizedBox(
+              height: 5,
+            ),
+            Center(
+              child: Container(
+                padding: EdgeInsets.all(10),
+                decoration: BoxDecoration(
+                    color: Colors.brown.shade300,
+                    borderRadius: BorderRadius.circular(50)),
+                width: MediaQuery.of(context).size.width * 0.4,
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Icon(
+                      Icons.note_add,
+                    ),
+                    Text("VIEW RECIEPT")
+                  ],
+                ),
+              ),
+            ),
+            SizedBox(
+              height: 40,
+            ),
+            Container(
+              // height: 0,
+              decoration: BoxDecoration(
+                  color: Colors.grey[300],
+                  borderRadius: BorderRadius.circular(5)),
+              child: ListTile(
+                contentPadding: EdgeInsets.symmetric(
+                  horizontal: 0,
+                ),
+                // leading: Icon(
+                //   Icons.balance,
+                //   size: 40,
+                // )
+                // ,
+                leading: SizedBox(
+                  width: 40,
+                  child: Image.asset(
+                    "images/cbe_logo.png",
+                    fit: BoxFit.fill,
+                  ),
+                ),
+                title: Text(
+                  "Commercial Bank of Ethiopia",
+                  style: TextStyle(fontWeight: FontWeight.bold, fontSize: 14),
+                ),
+                subtitle: Text(
+                  "The Bank You always Rely on!",
+                  style: TextStyle(
+                    color: Colors.grey,
+                    fontSize: 12,
+                  ),
+                ),
+              ),
+            )
+          ],
         ),
       ),
     );
